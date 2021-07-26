dd285d8b0ec886d80dd46ca4618ee5c43cbbd209
arodde <roddealain@gmail.comgit config --global user.email roddealain@gmail.comgit config --global user.email roddealain@gmail.com>
2021-07-26 12:29:31 +0200

    retraits commentaires inutiles

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -5,22 +5,6 @@ class CalculatorBrain {
   final int height;
   final int weight;
   late final double _bmi;
-  String deseases_risk_en = "BMI Interpretation Risk of disease"+
-      "Below 15 Extremely Important Famine"+
-      "15 to 18.5 Increased thinness"+
-      "18.5 to 25 Average / normal build Low"+
-      "25 to 30 Increased overweight"+
-      "30 to 35 Moderate obesity High"+
-      "35 to 40 Severe obesity Very high"+
-      "Over 40 Morbid obesity Extremely severe";
-  String deseases_risk_fr = "IMC Interprétation Risques de maladies "+
- " Au-dessous de 15 Famine Extrêmement important"+
-  "15 à 18,5 Maigreur Accrus"+
-  "18,5 à 25 Corpulence moyenne/normale Faibles"+
-  "25 à 30 Surpoids Accrus"+
-  "30 à 35 Obésité modérée Élevés"+
-  "35 à 40 Obésité sévère Très élevée"+
-  "Supérieur à 40 Obésité morbide Extrêmement important";
 
   String calculateBMI() {
     _bmi = weight / pow(height / 100, 2);
